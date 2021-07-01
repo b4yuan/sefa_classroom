65709e985f349f82f123f3cad4fec8fa69a0ef96
Leila Yanni <85317500+lvy15@users.noreply.github.com>
2021-07-01 10:53:43 -0400

    Add files via upload

---
+++ calcHoursLate.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from datetime import timedelta
+
+def calcHoursLate(subDate, dueDate):
+    #date format: year, month, day, hour, minute, second
+    #24 hour clock, must be padded with zeroes
+    #example: "2021-07-02 23:59:59"
+    FMT = '%Y-%m-%d %H:%M:%S'
+    timeDiff = datetime.strptime(dueDate, FMT) - datetime.strptime(subDate, FMT) #calculate time difference
+    timeDiff = timedelta.total_seconds(timeDiff) #convert difference to seconds
+    timeDiff = timeDiff / 3600 #convert difference to hours
+    return timeDiff
