a9f0a4833fcced8f8770dc6a6ea0ba09e7d9da2c
Leila Yanni <lyanni@purdue.edu>
2021-07-05 19:33:31 -0400

    changed repoClone to specify hw name

--- repoClone.py
+++ repoClone.py
@@ -6,12 +6,14 @@ import shutil
 import calcHoursLate
 import gitCurl
 
-def cloneFromRepos(org, repositories, tagName, authName, authKey):
+def cloneFromRepos(org, students, hwName, tagName, authName, authKey): #changed repository to students and added hwName
     hoursLateArr = []
-    for repo in repositories:
+    for student in students:
+        repo = hwName + '-' + student
         tagList = gitCurl.fetchTags(org, repo, authName, authKey)
         if (tagName in tagList) and ('graded_ver' not in tagList):
             reposURL = "https://" + authKey + "@github.com/" + org + "/" + repo + ".git"
+            owd = os.getcwd()
             if os.path.isdir(os.getcwd() + "/clones") == False:
                 os.mkdir(os.getcwd() + "/clones")
             os.chdir(os.getcwd() + "/clones")
@@ -20,11 +22,13 @@ def cloneFromRepos(org, repositories, tagName, authName, authKey):
             tagStr = 'git log -1 --format=%ai ' + tagName
             info = subprocess.check_output(tagStr.split()).decode()
             subDate =info.split(' ')[0] + ' ' + info.split(' ')[1]
-            print(subDate)
+            #print(subDate)
             hoursLate = calcHoursLate.calcHoursLate(subDate, "2021-06-30 15:59:59") #still need to get the due date from somewhere
             hoursLateArr.append([repo, hoursLate]) #2d array with repository URL and number of hours late
+            os.chdir(owd)
     print(hoursLateArr)
 
 if __name__ == "__main__":
-    [repos, students, hws] = gitCurl.fetchLists(gitCurl.fetchRepos("cam2testclass", "myers395", "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"))
-    cloneFromRepos("cam2testclass", repos, "final_ver", "myers395", "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")
+    #shutil.rmtree('clones') #this doesnt work
+    [students, hws] = gitCurl.fetchLists(gitCurl.fetchRepos("cam2testclass", "myers395", "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")) #modified output since fetchLists outputs students and hws and not repository list
+    cloneFromRepos("cam2testclass", students, 'hw02sort', "final_ver", "myers395", "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")
