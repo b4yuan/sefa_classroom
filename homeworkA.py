47c246abcb8e92692330ba5b194c5258dffbbb6b
Kaden Scott Merrill <merrill8@ecegrid-thin6.ecn.purdue.edu>
2021-07-07 12:41:07 -0400

    fixed

--- homeworkA.py
+++ homeworkA.py
@@ -3,8 +3,9 @@ import json
 import os
 import re
 import shutil
-import calcHoursLate.py
+#import calcHoursLate
 
+#Important: this file uses the tab convention
 def getFlagFromJSON(JSONFile, flag):
 	flag = []
 	with open(JSONFile, "r") as JFile:
@@ -24,7 +25,7 @@ def getRepoCloneURLSFromJSON(JSONFile):
 	return repositoryURLS
 
 def pullFileFromAllRepos(repositories, fileToPull):
-    hoursLateArr = []
+	hoursLateArr = []
 	for reposURL in repositories:
 		splitURL = reposURL.split("/")
 		reposFileWithGit = splitURL[len(splitURL) - 1].split(".")
@@ -44,18 +45,18 @@ def pullFileFromAllRepos(repositories, fileToPull):
 				#clones the directory only if final tag exists and it hasn't been graded yet
 				process = subprocess.run(["git", "clone", str(reposURL)], check=True, stdout=subprocess.PIPE).stdout
                 
-                os.chdir(reposURL) #navigate to cloned repo
-                info = subprocess.check_output('git log -1 --format=%ai ver_1').decode()
-                subDate =info.split(' ')[0] + ' ' + info.split(' ')[1]
-                hoursLate = calcHoursLate(subDate, dueDate) #still need to get the due date from somewhere
-                hoursLateArr.append([reposURL, hoursLate]) #2d array with repository URL and number of hours late
+				os.chdir(reposURL) #navigate to cloned repo
+				info = subprocess.check_output('git log -1 --format=%ai ver_1').decode()
+				subDate =info.split(' ')[0] + ' ' + info.split(' ')[1]
+				hoursLate = calcHoursLate(subDate, dueDate) #still need to get the due date from somewhere
+				hoursLateArr.append([reposURL, hoursLate]) #2d array with repository URL and number of hours late
 
-    return hoursLateArr
+	return hoursLateArr
 
 def putGradesInRepos(rootDirGrades, fileName, userList, rootDirRepos, hwName):
-	for user in userlist:
-		srcPath = rootDirGrades + "/" + user + "/" + hwName + "/" + fileName
-		dstPath = rootDirRepos + "/" + user + "/" + hwName + "/" + fileName
+	for user in userList:
+		srcPath = rootDirGrades + "/" + user + "/" + fileName
+		dstPath = user
 		if os.path.exists(srcPath) and os.path.exists(dstPath):
 			shutil.copy(srcPath, dstPath)
 		else:
@@ -63,11 +64,12 @@ def putGradesInRepos(rootDirGrades, fileName, userList, rootDirRepos, hwName):
 
 def pushChangeToRepos(rootPath, fileName, userList, hwName):
 	for user in userList:
-		srcPath = rootPath + "/" + user + "/" + fileName
+		srcPath = user + "/" + fileName #rootPath + "/" + user + "/" + fileName
+
 		if os.path.exists(srcPath):
 			subprocess.run(["git", "add", srcPath], check=True, stdout=subprocess.PIPE).stdout
 			subprocess.run(["git", "commit", "-m", str("Grades updated for " + hwName + ".")], check=True, stdout=subprocess.PIPE).stdout
-			subprocess.run(["git", "push"], check=True, stdout=subprocess.PIPE).stdout
+			#subprocess.run(["git", "push"], check=True, stdout=subprocess.PIPE).stdout
 			subprocess.run(["git", "tag", "graded_ver"], check=True, stdout=subprocess.PIPE).stdout #adds graded version tag
 			subprocess.run(["git", "push", "origin", "graded_ver"], check=True, stdout=subprocess.PIPE).stdout #need to push the tag specifically, will not update tag with just a general push command
 		else:
@@ -77,11 +79,24 @@ def getTimesLateFromFile(JSONFile, dueDate):
 	timesLate = []
 	for tag in tags:
 		timesLate.append(calcHoursLate(tag, dueDate))
-        return timesLate
+	return timesLate
 def deleteAllRepos(repoFileNames):
 	for repos in repoFileNames:
 			subprocess.run(["rm", repos], check=True, stdout=subprocess.PIPE).stdout 
 def startGradingProcess(runFilePath):
-	subprocess.run(["python3", "pas_backend/run_grader.py"], check=True, stdout=subprocess.PIPE).stdout
+	subprocess.run(["python3", runFilePath], check=True, stdout=subprocess.PIPE).stdout
+	testFile = "testJSON.json"
+testFile = "testJSON.json"
+testHW = "HW02Sort"
+runFile = "run_grader.py"
+gradeName = "grade.txt"
+gradeRoot = "grades"
+repoNames = getRepoNamesFromJSON(testFile)
+userURLS = getRepoCloneURLSFromJSON(testFile)
+pullFileFromAllRepos(userURLS, testHW)
+#getTimesLateFromFile(testFile, dueDate)
+startGradingProcess(runFile)
+putGradesInRepos(gradeRoot, gradeName, repoNames, repoNames, testHW)
+pushChangeToRepos("repos", gradeName, repoNames, testHW)
+deleteAllRepos(repoNames)
 	
-#pullFileFromAllRepos(getRepoCloneURLSFromJSON(example.json), "HW01Linux")
