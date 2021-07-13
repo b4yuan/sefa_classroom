fdd964ac4b08d8c0d141c9a1a856b1bc0953f945
Leila Yanni <lyanni@purdue.edu>
2021-07-13 17:23:20 -0400

    updated grading process

--- combinedSystem/functions/startGradingProcess.py
+++ combinedSystem/functions/startGradingProcess.py
@@ -2,21 +2,26 @@ import subprocess
 import os
 import random #for testing purposes
 
-def startGradingProcess(runFilePath):
-	graded = interface.grade_submission(os.path.join(path, '2020homeworks/grade_testing/hw1-8/sort.zip'), os.path.join(path, '2020homeworks/HW02Sort'))
-	obj = GradedSubmission()
-	grade = obj.get_grade() #returns a float that is rounded to two decimals
-	feedback = obj.get_error_list() #returns a list
-	#need to write it to a file - grade and feedback in one file
-
-def startGradingProcessModified(repos, hoursLate):
-	#creates grade.txt as test
+def startGradingProcess(repos):
 	for repo in repos:
 		owd = os.getcwd()
 		path = owd + "/grades/" + repo
-		os.makedirs(path)
-		path = path + '/grade.txt' #gradeReport.txt
-		file1 = open(path, "w")
-		file1.write(str(random.randrange(0,100)/100.)) #random percentage for testing
-		file1.close()
-		print('grade.txt created at ' + path)
+		os.makedirs(path) #creates repository folder in grades folder
+		path = path + '/gradeReport.txt'
+
+		#graded = interface.grade_submission(os.path.join(path, '2020homeworks/grade_testing/hw1-8/sort.zip'), os.path.join(path, '2020homeworks/HW02Sort'))
+		#obj = GradedSubmission()
+		grade = 34.34; # obj.get_grade() #returns a float that is rounded to two decimals
+		feedback = ["error on line 2", "something else is wrong"] #obj.get_error_list() #returns a list
+
+		file = open(path, "w") #creates grade report file
+		file.write("Grade: ")
+		file.write(str(grade))
+		file.write('%\nFeedback: ')
+		for line in feedback:
+			file.write(line)
+			file.write(". ")
+		file.close()
+
+		print('gradeReport.txt created at ' + path)
+		#need to write it to a file - grade and feedback in one file
