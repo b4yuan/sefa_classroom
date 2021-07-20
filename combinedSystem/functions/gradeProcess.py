9f1cd79494de84326c2c2dd941cc5de53260505d
Jack <myers395@purdue.edu>
2021-07-20 16:36:39 -0400

    fetchHWinfo reference wrong

--- combinedSystem/functions/gradeProcess.py
+++ combinedSystem/functions/gradeProcess.py
@@ -12,7 +12,7 @@ def cloneFromRepos(org, repo, hwNum, tagName, authName, authKey, profPath, clone
     owd = os.getcwd()
 
     subprocess.run(["git", "config", "--global", "advice.detachedHead", "false"], check=True) #Hide detatched head error
-    if fetchHWInfo(hwNum, repo):
+    if fetchHWInfo(hwNum, repo)[0]:
         tagList = fetchTags(org, repo, authName, authKey) #Get the tags for a specific repository
         
         if (tagName in tagList) and ('graded_ver' not in tagList): #If the repo is marked to be graded and hasn't already been graded
