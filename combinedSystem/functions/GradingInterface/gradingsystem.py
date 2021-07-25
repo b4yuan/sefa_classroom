80f49ff6e9f7f94533f203095783a45e3d2a95cb
student <agieson@purdue.edu>
2021-07-25 16:13:21 -0400

    fixed error where memcheck was timing out

--- combinedSystem/functions/GradingInterface/gradingsystem.py
+++ combinedSystem/functions/GradingInterface/gradingsystem.py
@@ -212,10 +212,12 @@ def memcheck(makefile_dir, valgrindstatements):
     for statement in valgrindstatements:  # run through the valgrind statements
         #os.system(f'valgrind {statement} > {tempfile} 2>&1')
         try:
-            checkfortimeout(os.system, args=[f'valgrind --tool=memcheck --log-file={tempfile} --leak-check=full --verbose {statement} >/dev/null 2>&1'])
+            checkfortimeout(os.system, args=[f'valgrind --tool=memcheck --log-file={tempfile} --leak-check=full --verbose {statement} >/dev/null 2>&1'], timeout=10)
             # previous statement executes valgrind on the executable and writes the output to the tempfile
             # also gets checked for a timeout obviously lmao
         except TimeoutError:
+            bytesInUse.append(0)
+            blocks.append(0)
             continue
         
         p = re.compile(r': ((\d*\,)*\d+) bytes in (\d+) blocks')  # regex for getting values from valgrind output
