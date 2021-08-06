708315a09f32f91bb1da32b98daf83647edd3647
Jack <myers395@purdue.edu>
2021-08-05 22:15:26 -0400

    commented csv code

--- combinedSystem/functions/dataFrameHelper.py
+++ combinedSystem/functions/dataFrameHelper.py
@@ -8,30 +8,30 @@ def updateDF(hws, students, df):
     #Takes in existing dataframe and returns the same dataframe with empty columns added
     #for each new entry in the student or homework list provided
 
-    if (df.empty):
+    if (df.empty): #If data is not provided create datafram with correct fields
         df= pd.DataFrame(np.zeros((len(students), len(hws))), columns= hws, dtype= float)
         df.insert(0, "GitHub Username", students)
 
     else:
-        for student1 in students:
-            presentFlag = False
-            for student2 in list(df["GitHub Username"]):
+        for student1 in students: #loop though students who's homework was graded
+            presentFlag = False #assume student is not in csv already
+            for student2 in list(df["GitHub Username"]): #students already in csv
                 if (student1 == student2):
-                    presentFlag = True
-            if (presentFlag == False):
-                newStudent= [{"GitHub Username":student1}]
-                for i in range(1, len(df.columns)):
+                    presentFlag = True 
+            if (presentFlag == False): #student is not found in csv
+                newStudent= [{"GitHub Username":student1}] #add student to username col
+                for i in range(1, len(df.columns)): #fill grades with zeros
                     newStudent[0][df.columns[i]]= 0
                 df= df.append(newStudent, ignore_index= True, sort= True)
 
-        oldHws = list(df.columns)
-        oldHws.remove("GitHub Username")
-        for hw1 in hws:
-            presentFlag = False
+        oldHws = list(df.columns) 
+        oldHws.remove("GitHub Username") #clean hw list from dataframe
+        for hw1 in hws: #hws provided
+            presentFlag = False #assume not in csv
             for hw2 in oldHws:
                 if (hw1 == hw2):
                     presentFlag = True
-            if (presentFlag == False):
+            if (presentFlag == False): #If hw is not in csv add with appropriate zero grades for all students
                 newHw= [0.0] * len(df.index)
                 df[hw1]= newHw
 
@@ -59,16 +59,16 @@ def editEntry(data, student, hw, df):
     #Takes a student and a homework assignment and assigns data to cooresponding data point
     #Returns updated data frame
 
-    if hw not in df.columns:
+    if hw not in df.columns: #hw isn't in dataframe
         print("Homework not present in dataframe.")
     else:
         condition = df["GitHub Username"] == student
         index = df.index
         studIndex = index[condition]
-        if (len(studIndex) != 1):
+        if (len(studIndex) != 1): #student isn't in dataframe
             print("Student not present in dataframe.")
         else:
-            df.loc[studIndex,hw] = data
+            df.loc[studIndex,hw] = data #edit homework assignment grade for student
 
     return df
 
